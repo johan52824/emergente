eb49cfe02ff52e643d4e62736232ce0e2655dca0
johan52824 <58148047+johan52824@users.noreply.github.com>
2023-05-16 15:49:27 -0500

    emergente

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer" style="background: #2a274b;color: #fff;">
+
+	<strong>SISTEMTAIPE &copy; 2021 <a href="https://tinyurl.com/y3ruj9rl" target="_blank">SISTEMAS COMPLETOS DE LA ULTIMA VERSION 2021</a>.</strong>
+	TODOS LOS DERECHOS RESERVADOS
+
+</footer>
